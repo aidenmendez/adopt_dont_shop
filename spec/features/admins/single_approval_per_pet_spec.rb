fd79004f755b37856e4c6955a4ad11d1b0094252
Aiden Mendez <aidenmendez@gmail.com>
2020-12-27 15:37:00 -0700

    Test that a pet can only be approved on a single application

--- spec/features/admins/single_approval_per_pet_spec.rb
+++ spec/features/admins/single_approval_per_pet_spec.rb
@@ -12,25 +12,38 @@ RSpec.describe "As a user" do
   end
 
   describe "when I am approving applications" do
-      it "and a pet has an Approved application, the admin application reflects the application approval" do
-        visit "/admin/applications/#{@application1.id}"
-        
-        within("#pet-#{@pet1.id}") do
-          click_button("Approve")
-        end
-        visit "/admin/applications/#{@application1.id}"
-        expect(page).to have_content("Approved")
+    it "a pet can only be approved on a single application" do
+      visit "/admin/applications/#{@application1.id}"
+      
+      within("#pet-#{@pet1.id}") do
+        click_button("Approve")
       end
 
-    describe "and I visit the admin application show page for a pending application" do
-      it "there is a message that the pet has already been approved for adoption and there is a only an option to reject it" do
-        visit "/admin/applications/#{@application2.id}"
+      visit "/admin/applications/#{@application1.id}"
+
+      expect(page).to have_content("Approved")
+
+      visit "/admin/applications/#{@application2.id}"
+
       within("#pet-#{@pet1.id}") do
+        save_and_open_page
         expect(page).to have_content("This pet has already found a home.")
-        expect(page).to have_content("Reject")
+        expect(page).to have_button("Reject")
         expect(page).not_to have_content("Approve")
-        end
       end
     end
   end
 end
+
+    # describe "and I visit the admin application show page for a pending application" do
+    #   it "there is a message that the pet has already been approved for adoption and there is a only an option to reject it" do
+    #     visit "/admin/applications/#{@application2.id}"
+
+    #     within("#pet-#{@pet1.id}") do
+    #       expect(page).to have_content("This pet has already found a home.")
+    #       expect(page).to have_content("Reject")
+    #       expect(page).not_to have_content("Approve")
+    #     end
+  #     end
+  #   end
+  # end
