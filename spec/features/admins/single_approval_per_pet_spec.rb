c1a004f15693643f2a39b09a8c75fd7c1d135fe5
Aiden Mendez <aidenmendez@gmail.com>
2020-12-28 13:13:39 -0700

    Update single_approval test

--- spec/features/admins/single_approval_per_pet_spec.rb
+++ spec/features/admins/single_approval_per_pet_spec.rb
@@ -2,6 +2,11 @@ require 'rails_helper'
 
 RSpec.describe "As a user" do
   before(:all) do
+    PetApplication.destroy_all
+    Pet.destroy_all
+    Application.destroy_all
+    Shelter.destroy_all
+
     @shelter = FactoryBot.create(:shelter)
     @pet1 = FactoryBot.create(:pet)
     @application1 = FactoryBot.create(:application)
@@ -26,7 +31,7 @@ RSpec.describe "As a user" do
       visit "/admin/applications/#{@application2.id}"
 
       within("#pet-#{@pet1.id}") do
-        save_and_open_page
+        
         expect(page).to have_content("This pet has already found a home.")
         expect(page).to have_button("Reject")
         expect(page).not_to have_content("Approve")
@@ -34,16 +39,3 @@ RSpec.describe "As a user" do
     end
   end
 end
-
-    # describe "and I visit the admin application show page for a pending application" do
-    #   it "there is a message that the pet has already been approved for adoption and there is a only an option to reject it" do
-    #     visit "/admin/applications/#{@application2.id}"
-
-    #     within("#pet-#{@pet1.id}") do
-    #       expect(page).to have_content("This pet has already found a home.")
-    #       expect(page).to have_content("Reject")
-    #       expect(page).not_to have_content("Approve")
-    #     end
-  #     end
-  #   end
-  # end
